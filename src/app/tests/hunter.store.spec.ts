e39d467e3432acf99aa6ca25da6508421eb19303
JTend <joseg.tenderor@gmail.com>
2021-01-23 22:34:40 -0400

    finished hunter tests

--- src/app/tests/hunter.store.spec.ts
+++ src/app/tests/hunter.store.spec.ts
@@ -4,7 +4,7 @@ import AppState from '../models/appstate';
 import Hunter from '../models/hunter';
 import * as action from '../store/hunter/actions';
 import * as fromHunter from '../store/hunter/reducer';
-import { selectHunter, selectHunterArrows, selectHunterDirection, selectHunterPosition, selHunterDirection } from '../store/hunter/selectors';
+import { selectHunter, selectHunterArrows, selectHunterDirection, selectHunterPosition } from '../store/hunter/selectors';
 
 describe('Hunter Store', () => {
   const initialState : Hunter = fromHunter.initialState;
@@ -25,15 +25,13 @@ describe('Hunter Store', () => {
     it('should have starting position [0,0]', () => {
       expect( selectHunterPosition(state) ).toEqual( jasmine.objectContaining({ X : 0, Y : 0 }) );
     });
-  
     it("should have 'UP' direction", () => {
       expect( selectHunterDirection(state) ).toBe('UP');
     });
-    
     it('should have 0 arrows', () => {
       expect( selectHunterArrows(state) ).toBe(0);
     });
-  });
+  });//describe Selectors
 
   describe('Reducer/Actions', () => {
     let newHunter : Hunter = { position : {X:5,Y:5}, direction : 'UP', arrows : 10 };
@@ -49,10 +47,13 @@ describe('Hunter Store', () => {
     });
   
     describe('changing directions by turning left', () => {
+      afterAll( () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.iniciarHunter({ hunter : newHunter }));
+      });
+
       it('should turn from UP to LEFT', () => {
         state.hunter = fromHunter.reducer(state.hunter, action.girarIzquierda());
-        console.info(selectHunterDirection(state));
-        expect( selHunterDirection(state) ).toEqual('LEFT');
+        expect( selectHunterDirection(state) ).toEqual('LEFT');
       });
       it('should turn from LEFT do DOWN direction', () => {
         state.hunter = fromHunter.reducer(selectHunter(state), action.girarIzquierda());
@@ -66,6 +67,69 @@ describe('Hunter Store', () => {
         state.hunter = fromHunter.reducer(selectHunter(state), action.girarIzquierda());
         expect( selectHunterDirection(state) ).toBe('UP');
       });
+    });//describe
+
+    describe('changing directions by turning right', () => {
+      afterAll( () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.iniciarHunter({ hunter : newHunter }));
+      });
+
+      it('should turn from UP to RIGHT', () => {
+        state.hunter = fromHunter.reducer(state.hunter, action.girarDerecha());
+        expect( selectHunterDirection(state) ).toEqual('RIGHT');
+      });
+      it('should turn from RIGHT do DOWN direction', () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.girarDerecha());
+        expect( selectHunterDirection(state) ).toBe('DOWN');
+      });
+      it('should turn from DOWN do LEFT direction', () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.girarDerecha());
+        expect( selectHunterDirection(state) ).toBe('LEFT');
+      });
+      it('should turn from LEFT do UP direction', () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.girarDerecha());
+        expect( selectHunterDirection(state) ).toBe('UP');
+      });
+    });//describe
+
+    describe('Advancing positions', () => {
+      beforeEach(() => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.avanzarPosicion());
+        state.hunter = fromHunter.reducer(selectHunter(state), action.girarDerecha());
+      });
+
+      afterAll( () => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.iniciarHunter({ hunter : newHunter }));
+      });
+
+      it('should have advanced to [5,6]', () => {
+        expect( selectHunterPosition(state) ).toEqual( jasmine.objectContaining({X:5,Y:6}));
+      });
+      it('should have advanced to [6,6]', () => {
+        expect( selectHunterPosition(state) ).toEqual( jasmine.objectContaining({X:6,Y:6}));
+      });
+      it('should have advanced to [6,5]', () => {
+        expect( selectHunterPosition(state) ).toEqual( jasmine.objectContaining({X:6,Y:5}));
+      });
+      it('should have advanced to [5,5]', () => {
+        expect( selectHunterPosition(state) ).toEqual( jasmine.objectContaining({X:5,Y:5}));
+      });
+    });//describe Advancing
+
+    describe('shooting 3 arrows', () => {
+      beforeEach(() => {
+        state.hunter = fromHunter.reducer(selectHunter(state), action.dispararFlecha());
+        state.hunter = fromHunter.reducer(selectHunter(state), action.dispararFlecha());
+      });
+      it('should have 8 arrows', () => {
+        expect( selectHunterArrows(state) ).toBe(8);
+      });
+      it('should have 6 arrows', () => {
+        expect( selectHunterArrows(state) ).toBe(6);
+      });
+      it('should have 4 arrows', () => {
+        expect( selectHunterArrows(state) ).toBe(4);
+      });
     });
-  });
-});
+  });//describe Reducer/actions
+});//describe Hunter store
